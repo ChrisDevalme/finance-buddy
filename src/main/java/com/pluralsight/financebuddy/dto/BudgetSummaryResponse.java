de2eff6822f8b9beecdc1372e47dedaed290ebc4
Chris Devalme <cdevalme@gmail.com>
2026-06-29 16:09:27 -0400

    Refactored Models with Lombok

--- src/main/java/com/pluralsight/financebuddy/dto/BudgetSummaryResponse.java
+++ src/main/java/com/pluralsight/financebuddy/dto/BudgetSummaryResponse.java
@@ -1,12 +1,13 @@
 package com.pluralsight.financebuddy.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class BudgetSummaryResponse {
 
     private Long budgetId;
